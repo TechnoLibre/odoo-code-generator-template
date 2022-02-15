1af8ebf82318b2994eee87040d5a5acb92a897c9
Mathieu Benoit <mathben@technolibre.ca>
2022-02-14 23:02:01 -0500

    [UPD] cg template demo_internal: create loop of improvement cg

--- code_generator_demo_internal/__manifest__.py
+++ code_generator_demo_internal/__manifest__.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
-
 {
     "name": "Setup testing environment for internal view code generator",
+    "category": "Uncategorized",
+    "version": "12.0.1.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
-    "depends": [
-        "code_generator",
-    ],
-    "data": [],
+    "depends": ["code_generator"],
     "installable": True,
     "post_init_hook": "post_init_hook",
     "uninstall_hook": "uninstall_hook",
--- demo_internal/__manifest__.py
+++ demo_internal/__manifest__.py
@@ -1,5 +1,6 @@
 {
     "name": "Demo internal",
+    "category": "Uncategorized",
     "version": "12.0.1.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
